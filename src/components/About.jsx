d136f9434edbea9627af00e9b17fc1e60db42185
nowakassoc <109696080+nowakassoc@users.noreply.github.com>
2023-03-26 14:58:13 -0500

    Update to About page content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,53 +5,33 @@ const About = () => (
     <span id="about" className="section-link" />
     <h2>About Martin Nowak</h2>
     <p>
-      Martin C. Nowak has more than thirty years experience in healthcare;
-      twenty three of those years at the executive level. From 2002 to 2005 he
+      Martin C. Nowak has more than thirty years' experience in healthcare;
+      twenty three of those years at the C-Suite level. From 2002 to 2005 he
       was Chief Planning and Strategy Officer for the UAB Health System; from
       1998 to 2002 he was interim then appointed Chief Executive Officer of UAB
       Hospital; and, from 1981 to 1998 he served in various executive hospital
       positions including Chief Executive Officer of St. Clair Regional Hospital
       and Administrator of the UAB Psychiatric Hospital. From 1992 to 1994 he
-      served as the Associate Executive Director for Information Systems (Chief
-      Information Officer) at UAB Hospital. With recognized success, Mr. Nowak
-      has directed hospitals from 90 to 908 beds; budgets exceeding $1.4 billion
-      revenue; and, workforce exceeding 5,000 employees. Mr. Nowak has
-      considerable consulting experience including distressed hospital
-      turnarounds.
+      served as the Associate Executive Director for Information Systems at UAB 
+      Hospital. With recognized success, Mr. Nowak has directed hospitals from 
+      90 to 1008 beds; budgets exceeding $1.5 billion revenue; and workforce 
+      exceeding 5,000 employees. Mr. Nowak has considerable consulting experience 
+      in all facets of healthcare delivery.
     </p>
     <p>
-      Mr. Nowak is cofounder and CEO of{' '}
-      <a
-        aria-label="ViM Analytics, LLC"
-        href="http://www.vimanalytics.com/"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        ViM Analytics, LLC
-      </a>
-      , a market research firm specializing in collecting and quantifying
-      hospital visitor experiences. Hospital visitors, which include caregivers,
-      family, friends, and even clergy members, are the most ignored demographic
-      in healthcare today but are eager participants in the caregiving process.
-      ViM&apos;s proprietary process utilizes the opinions and insights of
-      visitors to drive actionable change in healthcare settings. ViM findings
-      can be used to drive positive visitor experiences, which result in higher
-      satisfaction scores, improved community engagement, and ultimately,
-      improvements in volume and revenue.
-    </p>
-    <p>
-      He has served on numerous boards including the Alabama Hospital
+      Mr. Nowak has served on numerous boards including the Alabama Hospital
       Association from 2001 to 2005. The recipient of numerous awards, Mr. Nowak
       was named a &ldquo;Top 100 Who&apos;s Who in Healthcare&rdquo; by the
       Birmingham Business Journal (2002 and 2007) and received the
       &ldquo;Distinguished Service Award&rdquo; from the Alabama Hospital
-      Association (2002). Mr. Nowak is a frequent public speaker, and is often
+      Association (2002). Mr. Nowak is a frequent public speaker and is often
       sought for opinion by news media. He has been cited and interviewed on
       many occasions by healthcare trade journals. From 2003 - 2011 he served
       via gubernatorial appointment on the Alabama State Health Coordinating
       Council. He co-authored the health care strategic plan for indigent
       residents of Jefferson County, Alabama. Mr. Nowak is a Mentor in the UAB
-      Collat School of Business Innovation Depot.
+      Collat School of Business Innovation Depot and the UAB Graduate School 
+      of Health-Related Professionals.
     </p>
     <p>
       In 2005, he was named the first Outstanding Alumnus of UAB&apos;s School
@@ -60,10 +40,20 @@ const About = () => (
       was named UAB Distinguished Biology Alumnus.
     </p>
     <p>
+       Mr. Nowak is a consultant to venture funds by reviewing and providing 
+       subject matter expertise related to healthcare and other business opportunities.  
+       He advises and develops business plans and proposals for entrepreneurs and inventors.  
+       He provides advisory guidance to business start-ups and inventor's pursuit of 
+       placing intellectual property inventions into the marketplace.  Mr. Nowak 
+       works with law firms providing advisory and subject matter expertise in health-related matters, 
+       medical malpractice, and related cases.  From time to time, he also serves as an 
+       expert witness.
+    </p>
+    <p>
       Mr. Nowak has been a principle in several startup companies and is locally
       recognized for his entrepreneurial spirit. He holds several patents
-      including two medical claims data mining processes to illuminate quality
-      and other issues in care delivery. He has licensed another commercial
+      including two medical claims data mining processes to illuminate poor quality, 
+      fraud, and other issues in care delivery. He has licensed another commercial
       product patent to a national firm. Mr. Nowak earned both B.S. and M.H.A.
       degrees from the University of Alabama at Birmingham.
     </p>
