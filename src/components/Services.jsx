f5a12e4161f92889b8711a3844b16cc3babdeb23
nowakassoc <109696080+nowakassoc@users.noreply.github.com>
2023-03-26 14:22:28 -0500

    Update to services content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -9,7 +9,6 @@ const Services = () => (
     <ul className="service-list">
       <li>Strategic Planning</li>
       <li>Distressed Hospital Advice and Interim Management</li>
-      <li>Board and Executive Retreat Facilitation</li>
       <li>New Business Ventures and Startup</li>
       <li>Hospital - Physician Facilitation</li>
       <li>Request for Proposal Development</li>
@@ -19,12 +18,16 @@ const Services = () => (
     <ul className="service-list">
       <li>Business Plan Development</li>
       <li>Proforma Development</li>
+      <li>New business and inventor product advisory</li>
+      <li>Investment Proposal Review</li>
     </ul>
 
     <h3>Legal</h3>
     <ul className="service-list">
       <li>Hospital, Healthcare and Physician Practice Expertise</li>
       <li>Case Review, Expert Selection, Expert Preparation</li>
+      <li>Subject matter expertise and analysis of circumstances</li>
+      <li>Expert witness</li>
     </ul>
 
     <p>References Available Upon Request</p>
